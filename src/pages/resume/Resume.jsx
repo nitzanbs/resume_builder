aa0671097535be51db866f77e4c04139570f6f7b
nitzanbs <nitzanbs2248@gmail.com>
2024-01-30 22:55:47 +0200

    גרסה 1 עובדת של הוספת הטופס לפיירסטור

--- src/pages/resume/Resume.jsx
+++ src/pages/resume/Resume.jsx
@@ -1,139 +1,74 @@
-import React , { useState, useEffect , useContext } from 'react'
-import { getDocs, collection, query, where,addDoc, getDoc, deleteDoc, doc  } from "firebase/firestore";
+import React, { useState, useEffect, useContext } from 'react';
+import { getDocs, collection, query, where, addDoc } from "firebase/firestore";
 import { db } from '../../config/Config';
-
-import { UserContext } from '../../context/UserProvider'
-
+import { UserContext } from '../../context/UserProvider';
 import PersonalInfo from '../../component/PersonalInfo';
-import WorkExperience from '../../component/WorkExperience';
-import Education from '../../component/Education';
-
-
+// import WorkExperience from '../../component/WorkExperience';
+// import Education from '../../component/Education';
 
 export default function Resume() {
   const { user, setUser } = useContext(UserContext);
-
-  const [resume, setResume] = useState([])
-  const [resumeCard, setResumeCard] = useState({})
-
+  const [resume, setResume] = useState([]);
+  const [resumeCard, setResumeCard] = useState({});
   const [ErrMsg, setErrMsg] = useState("");
   const [addedResumeCardIds, setAddedResumeCardIds] = useState([]);
-
-
-
-  const resumeCollectionRef = collection(db, 'Resume')
+  const resumeCollectionRef = collection(db, 'Resume');
 
   const getResume = async () => {
-    try{
-      if(!user){
+    try {
+      if (!user) {
         return;
       }
-        const q = query(resumeCollectionRef, where("user","==",user.uid))
-        const rowDocs = await getDocs(q);
-        const docs = rowDocs.docs.map((doc) => {
-          return {...doc.data(), uid: doc.id };
-        });
-        setResume(docs);
-      }catch(err){
-        console.error(err);
-
-      }
-  }
-
-  console.log(user);
+      const q = query(resumeCollectionRef, where("user", "==", user.uid));
+      const rowDocs = await getDocs(q);
+      const docs = rowDocs.docs.map((doc) => ({ ...doc.data(), uid: doc.id }));
+      setResume(docs);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   useEffect(() => {
-    getResume()
-  }, [user])
+    getResume();
+  }, [user]);
 
   const changeHandler = (e) => {
-    setResume(prevResume => ({ ...prevResume, [e.target.name]: e.target.value }));
+    setResumeCard((prevData) => ({ ...prevData, ...{ [e.target.name]: e.target.value } }));
   };
   
-  // ...
-  
-const submitHandler = async (e) => {
-  e.preventDefault();
-
-  try {
-    const isAlreadyAdded = resume.some((item) => item.id === resumeCard.uid);
-
-    if (!isAlreadyAdded) {
-      const resumeCardRef = doc(resumeCollectionRef, resumeCard.uid);
-      await setDoc(resumeCardRef, {
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const resumeCardDoc = await addDoc(resumeCollectionRef, {
         ...resumeCard,
         isAdded: true,
         user: user.uid,
       });
+      console.log("Resume Card Doc:", resumeCardDoc);
 
-      setResume((prevResume) => [...prevResume, resumeCard]);
-      setAddedResumeCardIds([...addedResumeCardIds, resumeCard.id]);
+      setResume([]); // Clear the resume state
+      setResumeCard({});
+      setAddedResumeCardIds([...addedResumeCardIds, resumeCard.uid]);
+    } catch (error) {
+      console.error(error);
+      setErrMsg("Something went wrong");
     }
-  } catch (error) {
-    setErrMsg("Something went wrong");
-  }
-};
+  };
 
-  
-
-  // const RemoveRsume = async (resume) => {
-  //   console.log(resume.id);
-  //   try {
-  //     const q = query(
-  //       resumeCollectionRef,
-  //       where("id", "==", resume.id),
-  //       where("user", "==",user.uid)
-  //     );
-
-  //     const resumeCardDoc = await getDocs(q);
-
-  //     console.log(resumeCardDoc);
-  //     await deleteDoc(doc(db, "resume", resumeCardDoc.docs[0].id));
-
-  //     const filtered = resume.filter((item) => item.id !== resume.id);
-
-  //     setResume(filtered);
-
-
-  //     setResumeCard((prevResumeCard) =>
-  //     prevResumeCard.map((item) =>
-  //         item.id === resume.id ? { ...item, isAdded: false } : item
-  //       )
-  //     );
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-
-
-
-
-
-        console.log(resume);
+  console.log(resume);
+  console.log("resumeCard:", resumeCard);
 
   return (
-    
     <>
       <div>
         <h1>Resume</h1>
-
-        <form onSubmit={submitHandler} >
+        <form onSubmit={submitHandler}>
           <PersonalInfo changeHandler={changeHandler} />
-          <WorkExperience changeHandler={changeHandler}/>
-          <Education changeHandler={changeHandler}/>
+          {/* <WorkExperience changeHandler={changeHandler}/> */}
+          {/* <Education changeHandler={changeHandler}/> */}
           <button type="submit">send form</button>
-          
-
-
-
         </form>
-
-
-    
       </div>
-
-
     </>
-
-  )
+  );
 }
